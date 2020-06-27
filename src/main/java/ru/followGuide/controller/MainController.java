2a8ac3597b21a7c70d1f6b90de685d54835f1d0b
Maxim Yakovishin <yakovishin.maks@mail.ru>
2020-06-27 22:13:08 +0300

    Add file loader

--- src/main/java/ru/followGuide/controller/MainController.java
+++ src/main/java/ru/followGuide/controller/MainController.java
@@ -1,20 +1,28 @@
 package ru.followGuide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import ru.followGuide.domain.Message;
 import ru.followGuide.domain.User;
 import ru.followGuide.repositories.MessageRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     private MessageRepository messageRepository;
@@ -41,8 +49,20 @@ public class MainController {
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Map<String, Object> model) {
+                      @RequestParam MultipartFile file,
+                      Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+        if (file != null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile =UUID.randomUUID().toString();
+            String resultFileMessage = uuidFile.concat(".").concat(file.getOriginalFilename());
+            file.transferTo(new File(uploadPath + "/" + resultFileMessage));
+            message.setFilename(resultFileMessage);
+        }
             messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
--- src/main/java/ru/followGuide/domain/Message.java
+++ src/main/java/ru/followGuide/domain/Message.java
@@ -11,6 +11,7 @@ public class Message {
 
     private String text;
     private String tag;
+    private String filename;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
@@ -59,5 +60,14 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
