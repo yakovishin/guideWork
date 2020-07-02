ac2e34aa8ac4ca6139203d78cd6dd1a8ce52fd0b
Maxim Yakovishin <yakovishin.maks@mail.ru>
2020-07-02 18:35:15 +0300

    Add Mail

--- src/main/java/ru/followGuide/controller/RegistrationController.java
+++ src/main/java/ru/followGuide/controller/RegistrationController.java
@@ -4,18 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.followGuide.domain.Role;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.followGuide.domain.User;
-import ru.followGuide.repositories.UserRepository;
-
-import java.util.Collections;
-import java.util.Map;
+import ru.followGuide.service.UserService;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -24,14 +22,21 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Model model){
-        User userFromDB = userRepository.findByUsername(user.getUsername());
-        if (userFromDB != null){
+        if (!userService.addUser(user)){
             model.addAttribute("message", "User already exists");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        }else{
+            model.addAttribute("message", "Activation code is not found");
+        }
+        return "login";
     }
 }
--- src/main/java/ru/followGuide/repositories/UserRepository.java
+++ src/main/java/ru/followGuide/repositories/UserRepository.java
@@ -5,4 +5,6 @@ import ru.followGuide.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
