0a7b54e54054f740630a143138f92b135b20c094
Maxim Yakovishin <yakovishin.maks@mail.ru>
2020-07-09 19:24:48 +0300

    Add Message Editor

--- src/main/java/ru/followGuide/controller/MainController.java
+++ src/main/java/ru/followGuide/controller/MainController.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,8 +20,8 @@ import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -50,11 +51,13 @@ public class MainController {
     }
 
     @PostMapping("/index")
-    public String add(@AuthenticationPrincipal User user,
+    public String add(
+                      @AuthenticationPrincipal User user,
                       @Valid Message message,
                       BindingResult bindingResult,
                       @RequestParam MultipartFile file,
-                      Model model) throws IOException {
+                      Model model
+    ) throws IOException {
         message.setAuthor(user);
 
         if (bindingResult.hasErrors()) {
@@ -62,18 +65,7 @@ public class MainController {
             model.mergeAttributes(errorMap);
             model.addAttribute("message", message);
         }else {
-
-            if (file != null && !file.getOriginalFilename().isEmpty()) {
-                File uploadDir = new File(uploadPath);
-                if (!uploadDir.exists()) {
-                    uploadDir.mkdir();
-                }
-
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFileMessage = uuidFile.concat(".").concat(file.getOriginalFilename());
-                file.transferTo(new File(uploadPath + "/" + resultFileMessage));
-                message.setFilename(resultFileMessage);
-            }
+            saveFile(message, file);
             model.addAttribute("message", null);
             messageRepository.save(message);
         }
@@ -83,5 +75,54 @@ public class MainController {
         return "index";
     }
 
+    private void saveFile(@Valid Message message, @RequestParam MultipartFile file) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileMessage = uuidFile.concat(".").concat(file.getOriginalFilename());
+            file.transferTo(new File(uploadPath + "/" + resultFileMessage));
+            message.setFilename(resultFileMessage);
+        }
+    }
+
+    @GetMapping("/user-messages/{user}")
+    public String userMessages(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user,
+            @RequestParam(required = false) Message message,
+            Model model
+    ){
+        Set<Message> messages = user.getMessages();
+        model.addAttribute("messages", messages);
+        model.addAttribute("message", message);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+        return "userMessages";
+    }
+
+    @PostMapping("/user-messages/{user}")
+    public String updateMessage(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long user,
+            @RequestParam("id") Message message,
+            @RequestParam String text,
+            @RequestParam String tag,
+            @RequestParam MultipartFile file
+    ) throws IOException {
+        if(message.getAuthor().equals(currentUser)){
+            if (!StringUtils.isEmpty(text)){
+                message.setText(text);
+            }
+            if (!StringUtils.isEmpty(tag)){
+                message.setTag(tag);
+            }
+            saveFile(message, file);
+            messageRepository.save(message);
+        }
+        return "redirect:/user-messages/" + user;
+    }
 
 }
--- src/main/java/ru/followGuide/domain/User.java
+++ src/main/java/ru/followGuide/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -21,16 +22,22 @@ public class User implements UserDetails {
     private String password;
 
 
+
+
     private boolean active;
     @Email(message = "email is not correct")
     @NotBlank(message = "Email cannot be empty")
     private String email;
     private String activationCode;
 
+
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Message> messages;
 
     public boolean isAdmin(){
        return roles.contains(Role.ADMIN);
@@ -117,4 +124,24 @@ public class User implements UserDetails {
         this.activationCode = activationCode;
     }
 
+    public Set<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
